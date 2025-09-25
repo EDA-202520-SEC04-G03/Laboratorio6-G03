e043d566afdb7f9b11ae376da101e0fa647b8634
mateosanchez777 <masaza1039@gmail.com>
2025-09-25 11:52:09 -0500

    "funcion rehash y find_slot"

---
+++ DataStructures/Map/map_linear_probing.py
@@ -0,0 +1,28 @@
+import map_functions as mf
+
+def find_slot(my_map,key,hash_value):
+    index = hash_value(my_map,key)
+    i = 0
+    while my_map['entries'][(index + i) % my_map['capacity']] is not None:
+        if my_map['entries'][(index + i) % my_map['capacity']]['key'] == key:
+            return (index + i) % my_map['capacity'], True
+        i += 1
+    return (index + i) % my_map['capacity'], False
+
+
+def rehash(my_map):
+    old_entries = my_map['entries']
+    new_capacity = mf.next_prime(2 * my_map['capacity'])
+    my_map['capacity'] = new_capacity
+    my_map['prime'] = mf.next_prime(new_capacity + 1)
+    my_map['scale'] = mf.random.randint(1, my_map['prime'] - 1)
+    my_map['shift'] = mf.random.randint(0, my_map['prime'] - 1)
+    my_map['entries'] = [None] * new_capacity
+    my_map['size'] = 0
+
+    for entry in old_entries:
+        if entry is not None:
+            index, found = find_slot(my_map, entry['key'], mf.hash_value)
+            my_map['entries'][index] = entry
+            my_map['size'] += 1
+            
