530be88e418dd4ba7718b6b45277f40227637551
SantiagoEscoba23 <escobar.santiago00@gmail.com>
2025-09-25 11:54:30 -0500

    "Creacion new_map"

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -1,22 +1,24 @@
 import map_functions as mf
+import DataStructures.List.array_list as lt
 import map_entry as me
 import random
 
 def new_map(num_elements, load_factor, prime=109345121):
     capacity = mf.next_prime(int(num_elements / load_factor))
 
+    table = lt.new_list()
+    for _ in range(capacity):
+        lt.add_last(table, me.new_map_entry(None, None))
+
     hash_table = {
         "prime" : prime,
         "capacity" : capacity,
         "scale" : random.randrange(1, prime - 1),
         "shift" : random.randrange(0, prime - 1),
-        "table" : map.entry * capacity,
+        "table" : table,
         "current_factor" : 0,
         "limit_factor" : load_factor,
         "size" : 0
     }
 
     return hash_table
-
-my_table = new_map(5, 0.5)
-print(my_table)
